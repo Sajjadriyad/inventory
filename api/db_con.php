1b2101736f5508e682cfc04b15290bd7f6b6df2b
Sajjadriyad <sajjadhossainriyad146@gmail.com>
2023-11-03 05:41:39 -0700

    index complite

---
+++ api/db_con.php
@@ -0,0 +1,13 @@
+<?php 
+$hostname = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "inventory";
+
+// chak connection
+$con = new mysqli($hostname,$username,$password,$dbname);
+// chak erro
+
+if($con->connect_error){
+    echo "Oops! Datebase Connection Fail".$con->connect_error;
+}
